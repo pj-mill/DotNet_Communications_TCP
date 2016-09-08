f2bb15364aec6acf7878d7cd48445829ffb77a2d
Paul <paul.j.millar.1971@gmail.com>
2016-09-08 21:44:12 +0100

    Inital Commit

---
+++ BasicClient/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace BasicClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*-----------------------------------------------------------------------
+             * Title
+             * --------------------------------------------------------------------*/
+            string divider = new string('=', 100);
+            Console.WriteLine(divider);
+            Console.WriteLine("CLIENT");
+            Console.WriteLine(divider);
+
+
+            /*-----------------------------------------------------------------------
+             * Vars
+             * --------------------------------------------------------------------*/
+            int port = 43000;
+            TcpClient client;
+            byte count = 0;
+
+
+            /*-----------------------------------------------------------------------
+             * Send 3 messages (last one to close server)
+             * --------------------------------------------------------------------*/
+            while (count < 3)
+            {
+                try
+                {
+                    // Pause for 1 second.
+                    Thread.Sleep(1000);
+
+                    // Create a new client
+                    using (client = new TcpClient("localhost", port))
+                    {
+                        string msg = (count < 2) ? $"This is message {count + 1}" : "close";
+
+                        // Create a new stream to send message
+                        using (NetworkStream stream = client.GetStream())
+                        {
+                            using (BufferedStream writer = new BufferedStream(stream))
+                            {
+                                byte[] messageBytesToSend = Encoding.UTF8.GetBytes(msg);
+                                writer.Write(messageBytesToSend, 0, messageBytesToSend.Length);
+                                Console.WriteLine("Message sent...");
+                            }
+                        }
+                    }
+
+                    // Inc. counter
+                    count++;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Unable to connect to server");
+                    return;
+                }
+            }
+
+
+            /*-----------------------------------------------------------------------
+             * Finish up
+             * --------------------------------------------------------------------*/
+            Console.WriteLine("Client finished...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BasicServer/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace BasicServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*-----------------------------------------------------------------------
+             * Title
+             * --------------------------------------------------------------------*/
+            string divider = new string('=', 100);
+            Console.WriteLine(divider);
+            Console.WriteLine("SERVER");
+            Console.WriteLine(divider);
+
+
+            /*-----------------------------------------------------------------------
+             * Vars
+             * --------------------------------------------------------------------*/
+            TcpListener server;
+            Socket client;
+            NetworkStream stream = null;
+            IPAddress localhost = IPAddress.Parse("127.0.0.1");
+            int port = 43000;
+            int bytesToRead = 0;
+            int readByteSize = 10;
+            StringBuilder sb;
+            bool done = false;
+
+
+            /*-----------------------------------------------------------------------
+             * Initialise & start TCP listener
+             * --------------------------------------------------------------------*/
+            try
+            {
+                server = new TcpListener(localhost, port);
+                server.Start();
+                Console.WriteLine($"Server has started on {localhost}:{port}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to connect server");
+                return;
+            }
+
+
+            /*-----------------------------------------------------------------------
+             * Listen for clients on network
+             * --------------------------------------------------------------------*/
+            while (!done)
+            {
+                Console.WriteLine("Waiting for a connection...");
+                // Socket that manages the connection to the client (blocks code execution until a client joins)
+                client = server.AcceptSocket();
+
+                // Client joined
+                Console.WriteLine("\nConnection accepted.");
+                stream = new NetworkStream(client);
+                sb = new StringBuilder();
+                try
+                {
+                    // Read messages in chunks of 1024 bytes
+                    do
+                    {
+                        byte[] chunks = new byte[readByteSize];
+                        bytesToRead = stream.Read(chunks, 0, chunks.Length);
+                        sb.Append(Encoding.UTF8.GetString(chunks));
+                    }
+                    while (bytesToRead != 0);
+
+                    // Output the entire messsage when done.
+                    string msg = sb.ToString().Trim();
+                    Console.WriteLine(msg);
+
+                    // Close connection if instructed to.
+                    if (msg.Contains("close"))
+                    {
+                        done = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                // Tidy up
+                stream.Close();
+                client.Close();
+            }
+
+
+            /*-----------------------------------------------------------------------
+             * Close Tcp Listener
+             * --------------------------------------------------------------------*/
+            server.Stop();
+            Console.WriteLine("Server closed...");
+            Console.ReadKey();
+        }
+    }
+}
